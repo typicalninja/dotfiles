170950c6d1fdf8c355f0ff6c79acd3fc366f811d
typicalninja <65993466+typicalninja@users.noreply.github.com>
2026-05-17 11:46:08 +0530

    chore: add nvim options

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,6 @@
+vim.opt.clipboard = 'unnamedplus'   -- use system clipboard
+vim.opt.mouse = 'a'                 -- allow the mouse to be used in nvim
+
+-- UI
+vim.opt.number = true               -- show absolute number
+vim.opt.relativenumber = true       -- add relative numbers on the left side
