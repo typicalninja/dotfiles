686b0c0f24faa9adf143da898247180f638af06b
typicalninja <65993466+typicalninja@users.noreply.github.com>
2026-05-17 13:45:31 +0530

    chore(nvim): switch default colorscheme to kanagawa and add neo-tree

--- .config/nvim/plugin/catppuccin.lua
+++ .config/nvim/plugin/catppuccin.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
 	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
 })
-
-vim.cmd("colorscheme " .. "catppuccin-mocha")
+-- Disabled, my default is kanagawa
+-- vim.cmd("colorscheme " .. "catppuccin-mocha")
---
+++ .config/nvim/plugin/neo-tree.nvim.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+  {
+    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+    version = vim.version.range('3')
+  }
+})
