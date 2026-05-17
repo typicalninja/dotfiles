6b7032e6a0056a86ba77838db7d26cae8e0ad5ad
typicalninja <65993466+typicalninja@users.noreply.github.com>
2026-05-17 13:15:38 +0530

    chore(nvim): add common dependencies

---
+++ .config/nvim/plugin/dependencies.lua
@@ -0,0 +1,6 @@
+-- Dependencies for other plugins
+vim.pack.add({
+	"https://github.com/nvim-lua/plenary.nvim",
+	"https://github.com/MunifTanjim/nui.nvim",
+	"https://github.com/nvim-tree/nvim-web-devicons"
+})
